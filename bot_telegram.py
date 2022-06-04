8e8063270eb03ed74236ca106f5aa893260f216b
Nikita_M <mashalov.ne@gmail.com>
2022-06-04 14:56:35 +0300

    Init

---
+++ bot_telegram.py
@@ -0,0 +1,11 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+import os
+
+bot = Bot(token=os.getenv('TOKEN'))
+dp = Dispatcher(bot)
+
+
+executor.start_polling(dp,skip_updates=True)
